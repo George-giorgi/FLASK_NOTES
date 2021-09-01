2e72b51bb086d1bb8ebdbef9953ba8b5ab4e57eb
George <GeorgeGiorgika@gmail.com>
2021-09-01 16:42:45 +0400

    connect

--- Database/reg_db.py
+++ Database/reg_db.py
@@ -74,7 +74,7 @@ class Database:
     
 
 class Note_Db:
-    def __init__(self, email, note, time, important = "NO"):
+    def __init__(self, email = None, note= None, time= None, important= None):
         self.email = email
         self.note = note
         self.time = time
@@ -99,12 +99,13 @@ class Note_Db:
     def add_note(self):
         conn = self.db_connect()
         cursor = conn.cursor()
+        
         _SQL = ("""INSERT INTO notes(user_email, note, insert_time, important) 
-                                VALUES (%s, %s, %s, %s)
-                                """)
+                                        VALUES (%s, %s, %s, %s)
+                                        """)
         values = (self.email, self.note, self.time, self.important)
         cursor.execute(_SQL, values)
-        
+                
         conn.commit()
         conn.close()
     
@@ -119,6 +120,35 @@ class Note_Db:
         
         conn.commit()
         conn.close()
+
+    def update_inp_cont(self, time, email, condition):
+        conn = self.db_connect()
+        cursor = conn.cursor()
+        if condition == "changeImportant":
+            _SQL = ("""UPDATE notes SET insert_time = %s, important = %s
+                        WHERE insert_time = %s and user_email = %s and note = %s """)
+            values = (self.time, self.important, time, email, self.note )
+            cursor.execute(_SQL, values)
+        elif condition == "changeContent":
+            
+            _SQL =("""UPDATE notes SET insert_time = %s, note = %s
+                    WHERE insert_time = %s and user_email = %s""")
+            values = (self.time, self.note, time, email, )
+            cursor.execute(_SQL, values)
+            print(self.note)
+        conn.commit()
+        conn.close()
+            
+    def get_all(self):
+        conn = self.db_connect()
+        cursor = conn.cursor()
+        cursor.execute("""SELECT * from notes 
+                        WHERE user_email = %s""",(self.email,))
+        data = cursor.fetchall()
+
+        return data
+
+
 
         
         
--- notes/note_db.py
+++ notes/note_db.py
@@ -1,4 +1,4 @@
-from flask import Blueprint ,  request 
+from flask import Blueprint ,  request ,jsonify
 from Database.reg_db import Note_Db
 
 bp_todo_db = Blueprint("notes", __name__, url_prefix="/")
@@ -8,10 +8,18 @@ bp_todo_db = Blueprint("notes", __name__, url_prefix="/")
 def todo_db():
 
     if request.method == "GET":
-        user_email = request.args["user_email"]
-        # am emailis mixedvit wamovigheb bazidan dasarendereblad
-
-        return "get all notes"
+        send_data = []
+        user_email = request.args["user"]
+        data = Note_Db(user_email, None,None,None).get_all()
+        for i in data:
+            ins_data ={
+               "email": i[0],
+               "content": i[1],
+               "time": i[2],
+               "important": i[3]
+            }
+            send_data.append(ins_data)
+        return jsonify(send_data)  
     elif request.method =="POST":
         
         user_email =  request.json["user_email"]
@@ -19,18 +27,23 @@ def todo_db():
         insert_time =  request.json["insert_time"]
         important = request.json["important"]
         
-        Note_Db(user_email, note, insert_time,important ).add_note()
+        Note_Db(user_email, note, insert_time,important).add_note()
        
         return "add note sucsess"
     elif request.method =="PUT":
-        # aq unda daavabdato id is mixedvit, xolo redux shi gavyof sabolood gamogzavnistvis
-        user_email = request.json["email"]
-        note_id = request.json["id"]
-        note_content =  request.json["content"]
-        print(user_email, note_id, note_content)
+        print(request.json)
+        user_email = request.json["user"]
+        new_time = request.json["time"]
+        old_time = request.json["old_time"]
+        important = request.json["important"]
+        polimorfizm =  request.json["polimorfizm"]
+        content = request.json["content"]
+        
+        Note_Db(user_email, content, new_time , important ).update_inp_cont(old_time, user_email,polimorfizm )
+       
         return "update"
     elif request.method =="DELETE":
-        print(request.json)
+       
         time = request.json["time"]
         user = request.json["user"]
         Note_Db(user,  None, time , None ).del_note()
